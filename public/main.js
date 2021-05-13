2c48434c387e5d7ea8bc4f48e07fa5ee2d1dc8e1
Tomasz Łaski <tomasz.laski0001@gmail.com>
2021-05-13 23:00:58 +0200

    add save & load buttons with functionalities & routes

--- public/main.js
+++ public/main.js
@@ -16,3 +16,37 @@ function textPointer() {
 	var newNode = document.createElement("li");
 	selection.surroundContents(newNode);
 }
+
+function save() {
+	var xhttp = new XMLHttpRequest();
+	var note = document.getElementById("myiframe").innerHTML;
+	console.log(note, "note");
+	xhttp.open("POST", "http://localhost:3000/note", true);
+	xhttp.setRequestHeader("Content-Type", "application/json");
+	xhttp.onload = function () {
+		if (xhttp.status === 200) {
+			var userInfo = JSON.parse(xhttp.responseText);
+			console.log(userInfo, "userinfo");
+		}
+	};
+	xhttp.send(
+		JSON.stringify({
+			note: note,
+		})
+	);
+}
+
+function load() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("GET", "http://localhost:3000/note", true);
+	xhttp.onload = function () {
+		if (xhttp.status === 200) {
+			var response = JSON.parse(xhttp.responseText);
+			console.log(response, "res");
+			document.getElementById("myiframe").innerHTML = response.note;
+		} else {
+			console.log("error");
+		}
+	};
+	xhttp.send();
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const fs = require("fs");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post("/note", function (req, res) {
+	let data = JSON.stringify(req.body);
+	fs.writeFileSync("note.json", data);
+	res.json({ result: "OK" });
 });
 
+router.get("/note", function (req, res) {
+	let rawdata = fs.readFileSync("note.json");
+	let note = JSON.parse(rawdata);
+	res.json(note);
+});
 module.exports = router;
